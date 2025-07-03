35cc047745f44d65fbd92e2616396abda88b39e7
YASH-SAWARKAR <nitintaronejnt@gmail.com>
2025-07-03 22:33:00 +0530

    Initial commit

---
+++ 01DescribingTheUI/src/App.jsx
@@ -0,0 +1,25 @@
+// import Gallery from "./Gallery.jsx";
+// import { Profile } from "./Gallery.jsx";
+
+// export default function App() {
+//   return <Profile />;
+// }
+
+export default function App() {
+  return (
+    <>
+      <div class="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p class="summary">
+        You can find my thoughts here.
+        <br />
+        <br />
+        <b>
+          And <i>pictures</i>
+        </b>{" "}
+        of scientists!
+      </p>
+    </>
+  );
+}
